b7832f1b62a8764458fb01361caddf0d7de39c9a
Johann Cooper <johanngcooper@gmail.com>
2020-11-06 14:50:11 +0100

    added rejection when querying before connecting

--- src/database/index.ts
+++ src/database/index.ts
@@ -27,6 +27,11 @@ export const middleware = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export function queryDatabase(query: string): Promise<any> {
+	if (!isConnected) {
+		const err = new Error('A database connection must be established before querying');
+		return Promise.reject(err);
+	}
+
 	return new Promise((resolve, reject) => {
 		connection.query(query, (err, results) => {
 			if (err) reject(err);
