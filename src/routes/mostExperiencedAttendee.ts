23a6a82b872fd2294ca4d477e28acba7cd6f3245
Grant <grant.sutton@comcast.net>
2020-11-18 08:42:05 -0800

    added demo queries

---
+++ src/routes/mostExperiencedAttendee.ts
@@ -0,0 +1,39 @@
+import { Router, Request, Response } from 'express';
+import { queryDatabase } from '../database';
+
+export const router = Router();
+
+router.get('/mostExperiencedAttendee/:tripID', async (req: Request, res: Response) => {
+	try {
+		const { tripID: id } = req.params;
+
+		const results = await queryDatabase(`
+			SELECT t.full_name, t.tripCount
+			FROM (SELECT m.full_name, COUNT(*)
+				  FROM member m, attends_trip at1
+				  WHERE m.member_id = at1.member_id AND m.member_ID IN (SELECT member_ID
+				  														FROM atttends_trip at2
+				  														WHERE at2.trip_id = id)
+				  GROUP BY m.member_id) AS Temp
+			WHERE t.tripCount = (SELECT MAX(Temp.tripCount) FROM Temp;
+		`);
+
+		res.json({ data: results });
+	} catch (err) {
+		res.status(400);
+		res.json({ error: err });
+	}
+});
+
+/**
+WITH Temp(full_name, tripCount) AS
+	SELECT m.full_name, COUNT(*)
+	FROM member m, attends_trip attendT
+	WHERE m.member_id = attendT.member_id AND m.member_ID IN (SELECT member_ID
+															  FROM attends_trip at2
+															  WHERE at2.trip_id = id)
+	GROUP BY m.member_id
+SELECT t.full_name, t.tripCount
+FROM Temp t
+WHERE t.tripCount = (SELECT MAX(Temp.tripCount) FROM Temp;
+*/
